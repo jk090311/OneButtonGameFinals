93a8a602d4e131358af6ddf226d11393c8940df9
jk090311 <kirbyragasajo09@gmail.com>
2024-12-04 18:35:39 +0800

    12/04/2024 6pm - SettingsInProg

---
+++ Assets/Scripts/TitleScreenIdle.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class TitleScreenIdle : MonoBehaviour
+{
+    public float inactivityDuration = 10f;
+    public VideoPlayer videoPlayer;
+    public RawImage rawImage;
+    private float  inactivityTimer = 0f;
+    private bool isPlayingVideo = false;
+
+    void Start()
+    {
+        videoPlayer.gameObject.SetActive(false);
+        rawImage.enabled = false;
+    }
+
+    void Update()
+    {
+        if(Input.anyKeyDown || Input.GetMouseButton(0))
+        {
+            ResetInactivityTimer();
+        }
+        if(!isPlayingVideo)
+        {
+            inactivityTimer += Time.deltaTime;
+            if(inactivityTimer >= inactivityDuration)
+            {
+                PlayInactivityVideo();
+            }
+        }     
+    }
+    void ResetInactivityTimer()
+        {
+            inactivityTimer = 0f;
+            if(isPlayingVideo)
+            {
+                StopInactivityVideo();
+            }
+        }
+
+        void PlayInactivityVideo()
+        {
+            isPlayingVideo = true;
+            videoPlayer.gameObject.SetActive(true);
+            rawImage.enabled = true;
+            videoPlayer.Play();
+        }
+
+        void StopInactivityVideo()
+        {
+            isPlayingVideo = false;
+            videoPlayer.Stop();
+            videoPlayer.gameObject.SetActive(false);
+             rawImage.enabled = false;
+        }
+}
