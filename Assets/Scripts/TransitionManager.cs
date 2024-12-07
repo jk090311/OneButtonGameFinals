5db5ffb72aecc017e99994e68ef9365009a45601
jk090311 <kirbyragasajo09@gmail.com>
2024-12-07 21:40:38 +0800

    07/12/2024 9pm

--- Assets/Scripts/TransitionManager.cs
+++ Assets/Scripts/TransitionManager.cs
@@ -7,4 +7,6 @@ public class TransitionManager : MonoBehaviour
     public Animator animator;
   public void SettingsPageOpen (Animator animator) => animator.SetBool("isSettingsPageOpen", true);
   public void SettingsPageClose (Animator animator) => animator.SetBool("isSettingsPageOpen", false);
+  public void GameSelOpen (Animator animator) => animator.SetBool("isGameSelOpen", true);
+  public void GameSelClose (Animator animator) => animator.SetBool("isGameSelOpen", false);
 }
